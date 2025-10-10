33cdce2dfea49fe5a6e726dce87ffa14ef090e88
Shan <pruskimateusz93@gmail.com>
2025-10-10 11:12:24 +0200

    chore: WSL->Firefox Windows integration, explicitly using 127.0.0.1 host

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import node from "@astrojs/node";
 export default defineConfig({
   output: "server",
   integrations: [react(), sitemap()],
-  server: { port: 3000 },
+  server: { host: "127.0.0.1", port: 3000 },
   vite: {
     plugins: [tailwindcss()],
   },
