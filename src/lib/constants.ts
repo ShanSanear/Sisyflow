dc9d305464b147ddd6ce7d52178b664d5d930ae7
Shan <pruskimateusz93@gmail.com>
2025-10-13 19:16:59 +0200

    chore: update of user id after changing from using Windows drive on WSL to pure WSL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@
 
 // Stały user ID używany podczas developmentu
 // TODO: Usunąć gdy zostanie zaimplementowane pełne uwierzytelnienie
-export const DEVELOPMENT_USER_ID = "a66227cb-b970-4224-a6c0-f2ba668f136f";
+export const DEVELOPMENT_USER_ID = "e62f8bf5-c112-45c3-a0e5-59bd956fa319";
 
 // Limity załączników
 export const ATTACHMENT_LIMITS = {
