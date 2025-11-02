715fb7d562fd6731b208c8181d6ce79a94f5eac8
Shan <pruskimateusz93@gmail.com>
2025-11-02 10:13:42 +0100

    feat: proper login redirect in mail sent by supabase

--- src/pages/api/auth/sign-up.ts
+++ src/pages/api/auth/sign-up.ts
@@ -58,6 +58,9 @@ export const POST: APIRoute = async ({ request, cookies }) => {
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
+      options: {
+        emailRedirectTo: `${new URL(request.url).origin}/login`,
+      },
     });
 
     if (error) {
